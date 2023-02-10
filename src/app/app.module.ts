0d44c22c1c3b94b162fa871b3990b1ba15cc5dac
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-02-10 19:37:35 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavebarComponent } from './navebar/navebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavebarComponent
   ],
   imports: [
     BrowserModule
