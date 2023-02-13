135dc0ee878b027e0e974d18bf05f899033624ed
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-02-13 18:55:44 -0300

    criando a pasta dos principais componentes

--- src/app/principal/app.module.ts
+++ src/app/principal/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavebarComponent } from './navebar/navebar.component';
+import { NavebarComponent } from '../navebar/navebar.component';
 
 @NgModule({
   declarations: [
