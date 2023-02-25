7ed54f61a0d97c81636baba952685ba4fa2a3ed2
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-02-25 17:23:04 -0300

    criando o component dos herois

---
+++ src/app/herois/herois/herois.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-herois',
+  templateUrl: './herois.component.html',
+  styleUrls: ['./herois.component.css']
+})
+export class HeroisComponent {
+
+}
---
+++ src/app/herois/herois/herois.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroisComponent } from './herois.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeroisComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroisModule { }
