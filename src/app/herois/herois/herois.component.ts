23cfbb6a6392241fe8a8497b14e73c0968044371
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-05-22 15:21:00 -0300

    adicionando a consulta da api no componenete herois

--- src/app/herois/herois/herois.component.ts
+++ src/app/herois/herois/herois.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MarvelService } from 'src/app/api/marvel.service';
 
@@ -7,6 +7,19 @@ import { MarvelService } from 'src/app/api/marvel.service';
   templateUrl: './herois.component.html',
   styleUrls: ['./herois.component.css']
 })
-export class HeroisComponent {
+export class HeroisComponent implements OnInit{
+  heros: any[] = [];
 
+  constructor(private marvelService: MarvelService){}
+
+  ngOnInit(){
+    this.marvelService.getCharacters().subscribe(
+      (response: any) => {
+        this.heros = response.data.results;
+        console.log(this.heros);
+      }, (error: any) => {
+        console.error(error);
+      }
+    )
+  }
 }
