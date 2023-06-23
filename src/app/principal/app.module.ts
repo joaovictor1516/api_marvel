32dbcbde88f279133fca49ef3b25b23a0705d92d
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-06-23 16:20:51 -0300

    adicinando as rotas

--- src/app/principal/app.module.ts
+++ src/app/principal/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavebarComponent } from '../navebar/navebar.component';
 import { HeroisComponent } from '../herois/perssonagens/herois.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { HeroisComponent } from '../herois/perssonagens/herois.component';
     HeroisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
