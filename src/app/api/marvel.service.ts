91eca610781e698a7ca833fd29ccabafb2760cfb
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-06-23 16:17:04 -0300

    refatorando o codigo

--- src/app/api/marvel.service.ts
+++ src/app/api/marvel.service.ts
@@ -16,8 +16,11 @@ export class MarvelService {
   getCharacters(){
     const baseUrl:string = "https://gateway.marvel.com/v1/public/";
     const timeStemp:string = new Date().getTime().toString();
+
     const hash:string = CryptoJS.MD5(timeStemp + this.privateKey + this.publicKey).toString();
-    const url:string = `${baseUrl}/characters?ts=${timeStemp}&apikey=${this.publicKey}&hash=${hash}`
+
+    const url:string = `${baseUrl}/characters?ts=${timeStemp}&apikey=${this.publicKey}&hash=${hash}`;
+    
     const headers = new HttpHeaders().set("Content-Type", "application/json");
 
     return this.http.get(url, { headers });
