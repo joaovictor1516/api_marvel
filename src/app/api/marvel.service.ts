e654c94ebc497a255ff0ddbb63ec8359c16dd258
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-06-24 19:19:42 -0300

    adicionando a funcao responsavel pela busca das novels

--- src/app/api/marvel.service.ts
+++ src/app/api/marvel.service.ts
@@ -12,15 +12,21 @@ export class MarvelService {
   
   privateKey: string = "b570d67d539a3e9943771eb40333e5b45537f077";
   publicKey: string = "a7836e9b654b6019527d0921a05fb2dd";
+  baseUrl:string = "https://gateway.marvel.com/v1/public/";
+  timeStemp:string = new Date().getTime().toString();
+  hash:string = CryptoJS.MD5(this.timeStemp + this.privateKey + this.publicKey).toString();
 
   getCharacters(){
-    const baseUrl:string = "https://gateway.marvel.com/v1/public/";
-    const timeStemp:string = new Date().getTime().toString();
-
-    const hash:string = CryptoJS.MD5(timeStemp + this.privateKey + this.publicKey).toString();
-
-    const url:string = `${baseUrl}/characters?ts=${timeStemp}&apikey=${this.publicKey}&hash=${hash}`;
+    const url:string = `${this.baseUrl}/characters?ts=${this.timeStemp}&apikey=${this.publicKey}&hash=${this.hash}`;
     
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
+
+    return this.http.get(url, { headers });
+  }
+
+  getNovels(){
+    const url:string = `${this.baseUrl}/comics?ts=${this.timeStemp}&apikey=${this.publicKey}&hash=${this.hash}`;
+
     const headers = new HttpHeaders().set("Content-Type", "application/json");
 
     return this.http.get(url, { headers });
