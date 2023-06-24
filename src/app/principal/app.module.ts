7bae9d3ce2f2ae31da592ba694b67d014f046b6b
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-06-24 19:10:33 -0300

    importando o novo componente e a alteracao do nome do arquivo do componente herois e do componente navebar

--- src/app/principal/app.module.ts
+++ src/app/principal/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NavebarComponent } from '../navebar/navebar.component';
-import { HeroisComponent } from '../herois/perssonagens/herois.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
+import { NavebarComponent } from '../navebar/navebar.component';
+import { HeroisComponent } from '../herois/personagens/herois.component';
+import { NovelsComponent } from '../herois/novels/novels.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavebarComponent,
-    HeroisComponent
+    HeroisComponent,
+    NovelsComponent
   ],
   imports: [
     BrowserModule,
