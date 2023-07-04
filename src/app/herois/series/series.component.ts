d03905ee33976869fe9ad4aae1cef1ae68953a73
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-07-04 14:50:34 -0300

    adicao da funcionalidade de mostrar as series

--- src/app/herois/series/series.component.ts
+++ src/app/herois/series/series.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarvelService } from 'src/app/api/marvel.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-series',
   templateUrl: './series.component.html',
   styleUrls: ['./series.component.css']
 })
-export class SeriesComponent {
+export class SeriesComponent implements OnInit{
+  seriesHerois: any[] = [];
 
+  constructor(private MarvelService: MarvelService){}
+
+  ngOnInit(): void {
+      this.MarvelService.getSeries().pipe(take(1)).subscribe({
+        next: (response: any) => {
+          this.seriesHerois = response.data.results;
+          console.log(this.seriesHerois);
+        }, 
+        error: (error: any) => {
+          console.error(error);
+        }
+      })
+  }
+
+  
 }
