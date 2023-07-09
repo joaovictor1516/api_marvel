ea36ef7413a1eaad7ead237f50b0f3e3cf3d87fd
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-07-09 19:33:01 -0300

    atualizacao causada pela mudanca do nome da pasta do componente herois

--- src/app/principal/app.module.ts
+++ src/app/principal/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 import { NavebarComponent } from '../navebar/navebar.component';
-import { HeroisComponent } from '../herois/personagens/herois.component';
+import { HeroisComponent } from '../herois/herois/herois.component';
 import { NovelsComponent } from '../herois/novels/novels.component';
 import { SeriesComponent } from '../herois/series/series.component';
 import { PersonagemComponent } from '../herois/personagem/personagem.component';
