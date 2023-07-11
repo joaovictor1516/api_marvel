b29ec70e66f67d922dcd24229e6f9eef12c5729f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-07-10 22:26:58 -0300

    adicao da funcionalidade de pegar as informacoes do heroi passado pelo componente herois

--- src/app/herois/personagem/personagem.component.ts
+++ src/app/herois/personagem/personagem.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpHeaders,HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { MarvelService } from 'src/app/api/marvel.service';
 
 @Component({
   selector: 'app-personagem',
   templateUrl: './personagem.component.html',
   styleUrls: ['./personagem.component.css']
 })
-export class PersonagemComponent {
+export class PersonagemComponent implements OnInit{
+  personagemDados: any = [];
+  personagem: any = [];
+  
+  constructor(private marvelService: MarvelService, private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.personagem = this.marvelService.personagemSelecionado;
+
+    this.getCharacter().pipe(take(1)).subscribe({
+      next: (response: any) => {
+        this.personagemDados = response.data.results;
+        console.log(this.personagemDados, this.personagem);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    })
+  }
+
+  getCharacter(){
+    const url:string = `${this.marvelService.baseUrl}/characters/${this.personagem.id}?ts=${this.marvelService.timeStemp}&apikey=${this.marvelService.publicKey}&hash=${this.marvelService.hash}`;
+
+    const headers = new HttpHeaders().set("Content-Type", "application/json");
+
+    return this.http.get(url,{ headers });
+  }
 
 }
