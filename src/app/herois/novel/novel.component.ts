eb55db7964aa1054f8ce2beb169ce17b383acdfd
joaovictor1516 <voliveira.joao30@sempreceub.com>
2023-07-28 15:45:53 -0300

    melhorando o componente noovel

--- src/app/herois/novel/novel.component.ts
+++ src/app/herois/novel/novel.component.ts
@@ -1,38 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MarvelService } from 'src/app/api/marvel.service'; 
 import { take } from 'rxjs';
+import { Comic, Character } from 'src/app/interfaces/interfaces.component';
 
 @Component({
   selector: 'app-novel',
   templateUrl: './novel.component.html',
   styleUrls: ['./novel.component.css']
 })
-export class NovelComponent implements OnInit{
-  comic: any = [];
-  comicDados: any = []
 
-  constructor(private marvelService: MarvelService, private http: HttpClient){}
+export class NovelComponent implements OnInit{
+  comic: Comic = {} as Comic;
+  comicDados: Comic[] = [];
+  herois: Character[] = [];
+
+  constructor(private marvelService: MarvelService){}
 
   ngOnInit(): void {
       this.comic = this.marvelService.comicSelecionada;
 
-      this.getComic().pipe(take(1)).subscribe({
+      this.marvelService.getNovelsId(this.comic.id).pipe(take(1)).subscribe({
         next: (response: any) => {
-          this.comicDados = response.data.results
-          console.log(this.comicDados);
+          this.comicDados = response.data.results;
         },
         error: (error: any) => {
           console.error(error);
         }
       })
-  }
 
-  getComic(){
-    const url:string = `${this.marvelService.baseUrl}/comics/${this.comic.id}?ts=${this.marvelService.timeStemp}&apikey=${this.marvelService.publicKey}&hash=${this.marvelService.hash}`;
-
-    const headers = new HttpHeaders().set("Content-Type", "application/json");
-
-    return this.http.get(url,{ headers });
+      this.marvelService.getNovelsIdCharacters(this.comic.id).pipe(take(1)).subscribe({
+        next: (response: any) => {
+          this.herois = response.data.results;
+        },
+        error: (error: any) => {
+          console.error(error);
+        }
+      })
   }
 }
